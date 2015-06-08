bf80de94e9a01c3636a3b9ae974deeac7d172f67
Teymur Lennon Mardaliyev <teymur.oqtayoglu@gmail.com>
2015-06-08 06:05:01 +0500

    Create az-az.js
    Azerbaijani language

---
+++ src/flipclock/js/lang/az-az.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Azerbaijani Language Pack
+   *
+   * This class will used to translate tokens into the Azerbaijani language.
+   *
+   */
+
+  FlipClock.Lang.Azerbaijani = {
+
+    'years'   : 'il',
+    'months'  : 'ay',
+    'days'    : 'gün',
+    'hours'   : 'saat',
+    'minutes' : 'dəqiqə',
+    'seconds' : 'saniyə'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['az']      = FlipClock.Lang.Russian;
+  FlipClock.Lang['az-az']   = FlipClock.Lang.Russian;
+  FlipClock.Lang['azerbaijani']  = FlipClock.Lang.Russian;
+
+}(jQuery));
